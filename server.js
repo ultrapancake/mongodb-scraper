9b20ade175c69d2f02543ee6fd4b1d4505df52b3
Glen <ultrapancake@gmail.com>
2020-01-04 11:21:24 -0700

    set up body parser and express handlebars to the server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +9,22 @@ const app = express();
 const router = express.Router();
 
 app.use(express.static(__dirname + "/public"));
+
+//link handlebars and express app
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+//set up bodyParser
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 
 app.use(router);
 
